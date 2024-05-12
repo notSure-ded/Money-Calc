16549d1d2f872b681cd8e5576055f03940c1f869
notSure-ded <89379064+notSure-ded@users.noreply.github.com>
2024-05-12 15:52:08 +0530

    bonus  script.js

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ document.getElementById('sub').addEventListener("click",(event)=>{
   var days=document.getElementById('days');
   var money=document.getElementById('money');
   if(document.getElementById('check').checked){
-  calcTable(money.value,days.value)}
+    if(document.getElementById('bonus').checked)
+  calcTableB(money.value,days.value)
+   else calcTable(money.value,days.value); 
+  }
+  else if(document.getElementById('bonus').checked)
+    calcB(money.value,days.value)
   else calc(money.value,days.value)
 })
 
@@ -16,7 +21,6 @@ const calc=(m,d)=>{
   var profit=0.0;
   var result=" "
   for (let i = 0; i < d; i++) {
-    m+=600;
      invested=m*.1;
     profit=invested*.6;
     m=m+profit;
@@ -32,7 +36,6 @@ const calcTable = (m, d) => {
   var profit = 0.0;
 var result = '<table border="1"><tr><th>Current Money</th><th>Invested</th><th>Profit</th><th>Total Money</th></tr>';
   for (let i = 0; i < d; i++) {
-    m+=600;
     result+=`<tr><td>${m.toFixed(2)}</td><td>`
     invested = m * 0.1;
     profit = invested * 0.6;
@@ -42,4 +45,39 @@ var result = '<table border="1"><tr><th>Current Money</th><th>Invested</th><th>P
   result += `</table>`;
   result+=`<br> ${m.toFixed(2)}`;
 document.getElementById('res').innerHTML = result;
+}
+const calcTableB = (m, d) => {
+  m = parseFloat(m);
+  d = parseInt(d);
+  var invested = 0.0;
+  var profit = 0.0;
+var result = '<table border="1"><tr><th>Current Money</th><th>Invested</th><th>Profit</th><th>Total Money</th></tr>';
+  for (let i = 0; i < d; i++) {
+    m+=600;
+    result+=`<tr><td>${m.toFixed(2)}</td><td>`
+    
+    invested = m * 0.1;
+    profit = invested * 0.6;
+    m = m + profit;
+    result += `${invested.toFixed(2)}</td><td>${profit.toFixed(2)}</td><td>${m.toFixed(2)}</td></tr>`;
+  }
+  result += `</table>`;
+  result+=`<br> ${m.toFixed(2)}`;
+document.getElementById('res').innerHTML = result;
+}
+const calcB=(m,d)=>{
+  m = parseFloat(m);
+  d = parseInt(d);
+  var invested=0.0;
+  var profit=0.0;
+  var result=" "
+  for (let i = 0; i < d; i++) {
+    m+=600;
+     invested=m*.1;
+    profit=invested*.6;
+    m=m+profit;
+
+  }
+  result+=`${m.toFixed(3)}`
+  document.getElementById('res').innerHTML+=result;
 }
